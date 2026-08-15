79e2a1ca3dcd3c5d028c9ee7784e1abbd6dfc3fb
Your Name <nululavanya5@gmail.com>
2026-08-15 21:34:38 +0530

    "Fatorial of a number"

--- src/com/practice/Factorial.java
+++ src/com/practice/Factorial.java
@@ -1,7 +1,5 @@
 package com.practice;
-
 import java.util.Scanner;
-
 public class Factorial {
 
 	public static void main(String[] args) {
@@ -9,11 +7,10 @@ public class Factorial {
 		System.out.println("Enter n value:");
 		int n = sc.nextInt();
 		int fact = 1;
-		for (int i = 1; i <= n; i++) {
+		for(int i = 1; i<= n;i++) {
 			fact = fact * i;
 		}
-		System.out.println("Factorial is:"+ fact);
-
+		System.out.println("Factorial is:"+fact);
 	}
 
 }
