289cc28c9d4025b99af9906a498d34bd212356f2
Your Name <nululavanya5@gmail.com>
2026-08-17 19:54:21 +0530

    "missing values"

---
+++ src/com/Arrays/Missingvalues.java
@@ -0,0 +1,25 @@
+package com.Arrays;
+
+
+import java.util.Scanner;
+
+public class Missingvalues {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+
+        int sum = n * (n + 1) / 2;
+
+        System.out.println("Enter " + n + " numbers:");
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            sum = sum - num;
+        }
+
+        System.out.println("Missing number: " + sum);
+    }
+}
